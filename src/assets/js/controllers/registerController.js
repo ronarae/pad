3f367b32a97085b91430d033447c06a65da279b4
Niels Roeleveld <niels.roeleveld@hva.nl>
2020-04-21 09:51:29 +0200

    Merge

--- src/assets/js/controllers/registerController.js
+++ src/assets/js/controllers/registerController.js
@@ -58,29 +58,29 @@ class RegisterController {
         $(".content").html("Failed to load content")
     }
 
-    myApp = angular.module("myapp", []);
-    myApp.controller("PasswordController", function($scope) {
-
-        var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
-        var mediumRegex = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
-
-        $scope.passwordStrength = {
-            "float": "left",
-            "width": "100px",
-            "height": "23px",
-            "margin-left": "7px"
-        };
-
-        $scope.analyze = function (value) {
-            if (strongRegex.test(value)) {
-                $scope.passwordStrength["background-color"] = "green";
-            } else if (mediumRegex.test(value)) {
-                $scope.passwordStrength["background-color"] = "orange";
-            } else {
-                $scope.passwordStrength["background-color"] = "red";
-            }
-        };
-
-    }
+    // myApp = angular.module("myapp", []);
+    // myApp.controller("PasswordController", function($scope) {
+    //
+    //     var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    //     var mediumRegex = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
+    //
+    //     $scope.passwordStrength = {
+    //         "float": "left",
+    //         "width": "100px",
+    //         "height": "23px",
+    //         "margin-left": "7px"
+    //     };
+    //
+    //     $scope.analyze = function (value) {
+    //         if (strongRegex.test(value)) {
+    //             $scope.passwordStrength["background-color"] = "green";
+    //         } else if (mediumRegex.test(value)) {
+    //             $scope.passwordStrength["background-color"] = "orange";
+    //         } else {
+    //             $scope.passwordStrength["background-color"] = "red";
+    //         }
+    //     };
+    //
+    // }
 
 }
