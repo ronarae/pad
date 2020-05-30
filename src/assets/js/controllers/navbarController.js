f90873dc82309aa5f5c26c0b4511d16b543c5c64
W.F. Lam <wing.fung.lam@hva.nl>
2020-05-30 23:42:44 +0200

    correct navbar for login and logout

--- src/assets/js/controllers/navbarController.js
+++ src/assets/js/controllers/navbarController.js
@@ -13,22 +13,52 @@ class NavbarController {
     //Called when the navbar.html has been loaded
     setup(htmlData) {
         //Load the sidebar-content into memory
-        const sidebarView = $(htmlData);
+        this.sidebarView = $(htmlData);
 
         //Find all anchors and register the click-event
-        sidebarView.find("a").on("click", this.handleClickMenuItem);
+        this.sidebarView.find("a").on("click", this.handleClickMenuItem);
 
 
         //show user's name
-        sidebarView.find(".name").append(sessionManager.get("username"));
+        this.sidebarView.find(".name").append(sessionManager.get("username"));
 
         //When valid user is logged in, remove login option
         if(sessionManager.get("username")){
-            $("a[data-controller$='login']").remove();
+            console.log("navbar logic")
+            this.handleNavLogin();
+        }else{
+            this.handleNavLogout();
         }
 
+
+
         //Empty the sidebar-div and add the resulting view to the page
-        $(".sidebar").empty().append(sidebarView);
+        $(".sidebar").empty().append(this.sidebarView);
+    }
+
+    handleNavLogout(){
+        this.sidebarView.find(".name").hide();
+        this.sidebarView.find("a[data-controller$='logout']").hide();
+        this.sidebarView.find("a[data-controller$='contacts']").hide();
+        this.sidebarView.find("a[data-controller$='contactPage']").hide();
+        this.sidebarView.find("a[data-controller$='group']").hide();
+        this.sidebarView.find("a[data-controller$='groupPage']").hide();
+
+       this.sidebarView.find("a[data-controller$='login']").show();
+       this.sidebarView.find("a[data-controller$='register']").show();
+    }
+
+    handleNavLogin(){
+        this.sidebarView.find("a[data-controller$='login']").hide();
+        this.sidebarView.find("a[data-controller$='register']").hide();
+
+        this.sidebarView.find(".name").show();
+        this.sidebarView.find("a[data-controller$='logout']").show();
+        this.sidebarView.find("a[data-controller$='contacts']").show();
+        this.sidebarView.find("a[data-controller$='contactPage']").show();
+        this.sidebarView.find("a[data-controller$='group']").show();
+        this.sidebarView.find("a[data-controller$='groupPage']").show();
+
     }
 
     handleClickMenuItem() {
