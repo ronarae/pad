0faf4366b7b9bec4ec6ebc4cce67c87a754b265e
Yazan <yazan.mousa@hva.nl>
2020-06-03 16:04:41 +0200

    test

---
+++ server/cypress/integration/examples/pad/groupsAdd.js
@@ -0,0 +1,35 @@
+//  context: add group
+describe("addGroup", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:8080");
+    });
+    //test validate group addition form
+    it("Validate group addition form", () => {
+        //find the field for group name , check if it exists
+        cy.get("#inputGroupsName").should("exist");
+
+        //find the submit button , check if it exists
+        cy.get("#submit").should("exist");
+
+    });
+
+    //test succesful groud addition.
+    it("Successful group addition", () => {
+        //start a fake server
+        cy.server();
+
+        cy.route("POST", "/group").as("addGroup");
+
+        cy.get("#inputGroupsName").type("Boksen");
+
+        cy.get("#submit").click();
+
+        cy.wait("@addGroup");
+
+        cy.get(".error").should("exist").should("contain", "ERROR");
+
+
+
+    })
+
+})
